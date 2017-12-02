3342244945e45b074dec883f5547ef6558ee2092
apoorv74 <apoorv7491@gmail.com>
2017-12-03 00:33:36 +0530

    all matches data fetch

--- fetch_seasonal_data.R
+++ fetch_seasonal_data.R
@@ -1,16 +1,26 @@
-library(rvest)
-base_url <- "http://www.espncricinfo.com/ci/engine/series/index.html?season=2017;view=season"
-get_all_matches_cp <- ".series-summary-block"
 
+source("utils.R")
+library_list <- c("rvest","dplyr","stringr")
+lapply(library_list, require, character.only=TRUE)
+
+base_url <- "http://www.espncricinfo.com/ci/engine/series/index.html?season=2017;view=season"
+initial_url <- "http://www.espncricinfo.com"
+
+get_all_matches_cp <- ".series-summary-block"
 all_matches <- base_url %>% 
   read_html() %>% 
   html_nodes(css = get_all_matches_cp) %>% 
   html_node("a") %>% html_attr("href")
 
-initial_url <- "http://www.espncricinfo.com"
+# Including Test, ODI, T20Internationals and T20leagues
+all_matches <- all_matches[1:49]
+all_matches <- data.frame("match_category" = c(rep("Test",6),rep("ODI",14),rep("T20I",7),rep("T20L","4")),
+                          "series_link" = all_matches[c(1:27,46:49)], stringsAsFactors = F)
+
+
 master_table <- c()
-for(series_num in 1:6){
-url_series <- paste0(initial_url,all_matches[series_num])
+for(series_num in 1:nrow(all_matches)){
+url_series <- initial_url %+% all_matches$series_link[series_num]
 
 match_text <- url_series %>% 
   read_html() %>% 
@@ -20,9 +30,27 @@ match_text <- unlist(stringr::str_split(match_text,"\n\n\n\t\t\t"))
 match_text <- match_text[seq(2,length(match_text),2)]
 
 for(i in 1:length(match_text)){
+  
+  if(grepl("D/L method)",match_text[i])){
+    match_text_1 <- stringr::str_trim(
+      stringr::str_sub(match_text[i],1,
+                       data.frame(stringr::str_locate_all(match_text[i],"\\("))[4,1] - 1))  
+  } else {
+  if(grepl("with [0-9]* ball",match_text[i])){
   match_text_1 <- stringr::str_trim(
     stringr::str_sub(match_text[i],1,
-                          data.frame(stringr::str_locate(match_text[i],"\\(") - 1)[,1]))
+                          data.frame(stringr::str_locate_all(match_text[i],"\\("))[3,1] - 1))
+  } else{
+    if(nrow(data.frame(stringr::str_locate_all(match_text[i],"\\(")))==1){
+    match_text_1 <- stringr::str_trim(
+      stringr::str_sub(match_text[i],1,
+                       data.frame(stringr::str_locate(match_text[i],"\\(") - 1)[,1]))
+    } else {
+      match_text_1 <- stringr::str_trim(
+        stringr::str_sub(match_text[i],1,
+                         data.frame(stringr::str_locate_all(match_text[i],"\\("))[2,1] - 1))
+    } 
+    }}
  team_won <- stringr::str_trim(stringr::str_sub(match_text_1,1,stringr::str_locate(match_text_1," ")[[1]]))
  first_team <- stringr::str_trim(stringr::str_sub(match_text_1,
                                                   stringr::str_locate(match_text_1,"\n\n")[[1]],
@@ -30,7 +58,9 @@ for(i in 1:length(match_text)){
  second_team <- stringr::str_trim(stringr::str_sub(match_text_1,
                                                    stringr::str_locate(match_text_1,";")[[1]] + 1,
                                                    stringr::str_length(match_text_1)))
- match_stats <- data.frame("team_won"=team_won,"first_team"=first_team,"second_team"=second_team)
+ match_stats <- data.frame("team_won"=team_won,"first_team"=first_team,"second_team"=second_team,"category"=all_matches$match_category[series_num])
  master_table <- dplyr::bind_rows(master_table,match_stats)
+ 
 }
+print("Completed Series # " %+% as.character(series_num) %+% " which is a " %+% all_matches$match_category[series_num] %+% " series.")
 }
